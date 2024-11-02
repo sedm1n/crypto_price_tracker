1c21de63a03d7ce964cb16898fec25d25883bca1
sedm1n <4310481@gmail.com>
2024-11-02 09:31:00 +0300

    add settings db to config.py

---
+++ src/core/config.py
@@ -0,0 +1,31 @@
+from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    
+    MODE: Literal["DEV", "PROD", "TEST"]
+
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_PORT: int
+    DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_USER: str
+    TEST_DB_PASSWORD: str
+    TEST_DB_PORT: int
+    TEST_DB_NAME: str
+
+    SECRET_KEY: str
+
+    LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
+
+
+cfg = Config()
