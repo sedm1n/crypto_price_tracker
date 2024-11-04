b78c9d757ade3dbd5f745bad7caca28004464dfd
sedm1n <4310481@gmail.com>
2024-11-04 14:19:58 +0300

    redefined get_all in PriceHistoryDao, add filtered date, and add get_unix_timestamp function

--- src/db/dao/price_history.py
+++ src/db/dao/price_history.py
@@ -1,21 +1,55 @@
+import logging
 from datetime import datetime
-from typing import List, Optional
-from src.db.dao.ticker import TickerDao
-from src.db.models.price_history import PriceHistory
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.core.exeptions import PricesNotFoundError, TickerNotFoundError
 from src.db.dao.base import BaseDao
-from src.core.exeptions import TickerNotFoundError, PricesNotFoundError
+from src.db.dao.ticker import TickerDao
+from src.db.database import get_async_session
+from src.db.models.price_history import PriceHistory
+
+logger = logging.getLogger(__name__)
 
 
 def get_unix_timestamp(date_str: str) -> int:
-    
-    dt = datetime.strptime(date_str, "%d-%m-%Y")  
-    
+
+    dt = datetime.strptime(date_str, "%d-%m-%Y")
+
     return int(dt.timestamp())
+
 
 class PriceHistoryDao(BaseDao):
     model = PriceHistory
 
-    async def get_all_by_ticker(self, ticker: str) -> List[PriceHistory]:
+    @classmethod
+    async def get_all(
+        cls,
+        ticker_id: int = None,
+        start_timestamp: int = None,
+        end_timestamp: int = None,
+        **filter_by,
+    ):
+        async with get_async_session() as session:
+
+            query = select(cls.model).filter_by(**filter_by)
+            if ticker_id:
+                query = query.filter(cls.model.ticker_id == ticker_id)
+
+            if start_timestamp and end_timestamp:
+                query = query.filter(
+                    cls.model.created_at >= start_timestamp, cls.model.created_at < end_timestamp
+                )
+            try:
+                result = await session.execute(query)
+                return result.scalars().all()
+            except SQLAlchemyError as e:
+                logger.error(e, extra={"filter": filter_by}, exc_info=True)
+                return None
+
+    async def get_all_by_ticker(self, ticker: str) -> list[PriceHistory]:
 
         ticker = await TickerDao().get_by_ticker(name=ticker)
 
@@ -38,7 +72,10 @@ class PriceHistoryDao(BaseDao):
         sorted_prices = sorted(prices, key=lambda price: price.created_at, reverse=True)
 
         return sorted_prices[0]
-    async def get_by_ticker_and_date(self, ticker: str, start_date: str, end_date: str) -> Optional[PriceHistory]:
+
+    async def get_by_ticker_and_date(
+        self, ticker: str, start_date: str, end_date: str
+    ) -> Optional[PriceHistory]:
 
         ticker = await TickerDao().get_by_ticker(name=ticker)
 
@@ -48,11 +85,13 @@ class PriceHistoryDao(BaseDao):
         start_timestamp = get_unix_timestamp(start_date)
         end_timestamp = get_unix_timestamp(end_date)
 
-        prices = await PriceHistoryDao().get_all(ticker_id=ticker.id, created_at__gte=start_timestamp, created_at__lte=end_timestamp)
-        
+        prices = await self.get_all(
+            ticker_id=ticker.id,
+            start_timestamp=start_timestamp,
+            end_timestamp=end_timestamp,
+        )
 
         if not prices:
             raise PricesNotFoundError("Prices not found")
 
-        
         return prices
